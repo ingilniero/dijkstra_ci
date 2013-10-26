3b5788a5cdec4f16a204f08299a971645e5d4ce7
Gilberto Villa <ingilniero@gmail.com>
2013-10-25 21:56:39 -0500

    Add graph helpers to follow the flow of the algorithm

--- graph.rb
+++ graph.rb
@@ -1,6 +1,50 @@
 class Graph
-  def nodes
-    []
+  attr_reader :routes, :distances, :previous, :visited
+
+  def initialize
+    @routes = []
+    @distances = {}
+    @visited = {}
+    @previous = {}
   end
 
+  def add_path(node_1, node_2, cost)
+    node_1 = Node.new node_1
+    node_2 = Node.new node_2
+    @routes << Edge.new(node_1, node_2, cost)
+  end
+
+  def init(node)
+    distances[node] = 0
+    previous[node] = 'source'
+  end
+
+  def clear
+    routes.each do |route|
+      distances[route.destination.name] = 1000
+      previous[route.destination.name] = 0
+      visited[route.destination.name] = false
+    end
+  end
+
+  def check(node)
+    visited[node] = true
+  end
+
+
+
+  def overwrite_distance_for(node, neighbor, cost)
+    update_distance_for(neighbor, cost)
+    update_nodes(neighbor, node)
+  end
+
+  private
+
+  def update_distance_for(node, distance)
+    distances[node] = distance
+  end
+
+  def update_nodes(node, neighbor)
+    previous[node] = neighbor
+  end
 end
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -1,17 +1,53 @@
 require 'graph'
+require 'node'
+require 'edge'
+require 'pry'
 
 describe Graph do
-  let(:node_1) { Node.new }
-  let(:node_2) { Node.new }
-
-  it 'can create edge with nodes and a cost' do
-    expect(subject.set_edge(node_1, node_2, 3)).to be_true
-  end
+  let(:node_1) { '1' }
+  let(:node_2) { '2' }
 
   it 'has nodes' do
-    expect(subject.nodes).to be_kind_of(Array)
+    expect(subject.routes).to be_kind_of(Array)
   end
 
-  describe '#find' do
+  describe '#add_path' do
+    it 'creates and edge with nodes and a cost' do
+      expect(subject.add_path(node_1, node_2, 3)).to be_true
+    end
+  end
+
+  describe '#init' do
+    before do
+      subject.init('1')
+    end
+
+    it 'sets a tentative distance for the first node' do
+      expect(subject.distances['1']).to eq 0
+    end
+
+    it 'sets the previous node to a fix value' do
+      expect(subject.previous['1']).to eq 'source'
+    end
+  end
+
+  describe '#check' do
+    before { subject.check('1') }
+    it 'marks a node as visited' do
+      expect(subject.visited['1']).to be_true
+    end
+  end
+
+  describe '#overwrite_distance_for' do
+    before { subject.overwrite_distance_for('2', '1', 10) }
+
+    it 'updates the relationship between nodes' do
+      expect(subject.previous['1']).to eq '2'
+    end
+
+    it 'updates the distance of a node' do
+      expect(subject.distances['1']).to eq 10
+    end
+
   end
 end
